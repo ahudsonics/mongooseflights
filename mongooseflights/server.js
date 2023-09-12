11ae058413b6df75dc65ac192ca33258d8da7647
anthonydhudson <ahudson@icstars.org>
2023-09-12 18:42:45 -0500

    Update server.js

--- mongooseflights/server.js
+++ mongooseflights/server.js
@@ -6,3 +6,26 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/api/flights', async (req, res) => {
+  try {
+    const flights = await Flight.find().sort('departs').exec();
+    res.json(flights);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
